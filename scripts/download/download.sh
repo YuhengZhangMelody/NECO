eb84057575da378a24b32daefdd2220685653098
YuhengZhangMelody <melodyyuhengz@outlook.com>
2026-02-18 16:05:41 +0100

    更新下载脚本以支持多个数据集和检查点

--- scripts/download/download.sh
+++ scripts/download/download.sh
@@ -2,9 +2,8 @@
 
 # download the up-to-date benchmarks and checkpoints
 # provided by OpenOOD v1.5
-python ./scripts/download/download.py \
-	--contents 'datasets' 'checkpoints' \
-	--datasets 'ood_v1.5' \
-	--checkpoints 'ood_v1.5' \
-	--save_dir './data' './results' \
-	--dataset_mode 'benchmark'
+  python scripts/download/download.py \
+    --contents datasets \
+    --datasets cifar100 cifar10 tin mnist svhn texture places365 \
+    --save_dir ./data ./results \
+    --dataset_mode dataset
